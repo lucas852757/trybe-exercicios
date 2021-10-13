1b753e24c529e0fd9925e04d216419253af3f36f
Lucas <bithome1@outlook.com>
2021-10-13 13:28:04 -0300

    Exercicio 5 completo

---
+++ fundamentos/bloco-04-introducao-a-javascript-e-lógica-de-programacao/dia-1-javascript-primeiros-passos/triangulo.js
@@ -0,0 +1,19 @@
+let a = 15;
+let b = 12;
+let c = 9;
+
+if ((a + b) > c) {
+  if (( a + c) > b){
+    if ((b +c > a)) {
+      console.log("Triângulo")
+    }
+  }
+}else {
+  console.log("Não é triângulo")
+}
+/* Esse código recebe três variáveis a, b e c com os respectivos valores 15 ,12, 9. 
+A estrutura if contruída através de um conceito matemático que trata da condição 
+de  existência de um triângulo, verifica se os valores das variáveis a,b e c, são 
+condiçoes para a existẽncia do mesmo. Caso afirmativo o console.log imprime 
+"Triângulo", caso negativo imprime "Não é triangulo". */
+/* Source link: https://brasilescola.uol.com.br/matematica/triangulo.htm */
