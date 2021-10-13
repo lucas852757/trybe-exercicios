9a6f46a6f05d82fff83bbf76fec9be95e5aae9ba
Lucas <bithome1@outlook.com>
2021-10-13 15:41:39 -0300

    Exercicio 7 completo

---
+++ fundamentos/bloco-04-introducao-a-javascript-e-lógica-de-programacao/dia-1-javascript-primeiros-passos/conversor_de_notas.js
@@ -0,0 +1,14 @@
+nota = 50;
+if (nota >= 90){
+  console.log("A");
+}else if (nota >= 80) {
+  console.log("B");
+}else if (nota >= 70) {
+  console.log("C");
+}else if (nota >= "60") {
+console.log("D");
+}else if (nota >= "50") {
+  console.log("E");
+}
+/* O código acima converte notas de 0 a 90  dadas em porcentagem  
+em conceitos de A a F . */
