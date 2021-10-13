668ec307bebdeeebb94f98388adf276e24aff445
Lucas <bithome1@outlook.com>
2021-10-13 19:16:39 -0300

    Precisa ser corrigido falha!

---
+++ fundamentos/bloco-04-introducao-a-javascript-e-lógica-de-programacao/dia-1-javascript-primeiros-passos/exercicio10.js
@@ -0,0 +1,17 @@
+const custoDoProduto = 1000 + 20/100 * 1000;
+const valorDeVenda = 3000;
+
+let lucro = valorDeVenda - custoDoProduto;
+
+ let unidades = 1000;
+
+ if (unidades < 0 ) { 
+  console.log("Número inválido!");
+ }
+ else if (unidades == 1000) {
+  console.log("Lucro %d", unidades * lucro);
+}
+ /* O código calcula o lucro obtido pela venda de 
+ 1000 produtos. Se a variável unidades 
+ for negativa o programa 
+ exibe uma mensagem de erro e é encerrado .*/
