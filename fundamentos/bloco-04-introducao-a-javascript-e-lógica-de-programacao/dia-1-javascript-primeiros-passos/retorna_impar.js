81fe58a869ffdde6eba635a0487198d37465fbf8
Lucas <bithome1@outlook.com>
2021-10-13 18:27:11 -0300

    Exercicio 9 completo

---
+++ fundamentos/bloco-04-introducao-a-javascript-e-lógica-de-programacao/dia-1-javascript-primeiros-passos/retorna_impar.js
@@ -0,0 +1,13 @@
+const a = 1;
+const b = 2; 
+const c = 3;
+
+if ((((a %2) == 1 ) || (b % 2 ) == 1) || ((c % 2) == 1 )) {
+  console.log("true")
+}
+else {
+  console.log("false")
+}
+/* Este código recebe três constantes: a, b e c com seus respectivos valores 1, 
+2 e 3. Ele retorna true se pelo menos um valor for ímpar. Caso contrário,
+retorna false . */
