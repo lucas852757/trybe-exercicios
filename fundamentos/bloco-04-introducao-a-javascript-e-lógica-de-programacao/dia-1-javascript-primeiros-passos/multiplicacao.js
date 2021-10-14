83afb2798cd5656e62149b0557767ebd46e008e1
Lucas <bithome1@outlook.com>
2021-10-13 22:14:16 -0300

    Acrescentado exercicio de multiplicaçao

---
+++ fundamentos/bloco-04-introducao-a-javascript-e-lógica-de-programacao/dia-1-javascript-primeiros-passos/multiplicacao.js
@@ -0,0 +1,7 @@
+let a = 10;
+let b = 2;
+
+let resposta = 10 * 2;
+console.log("Resposta: %d", resposta);
+/* O código recebe as variáveis a e b com os respectivios 
+valores 10 e 2 e realiza uma multiplicação. */
