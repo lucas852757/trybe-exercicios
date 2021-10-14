d3ef5a8604fcc66d12b47f9745f5bd38a84e1c90
Lucas <bithome1@outlook.com>
2021-10-13 21:55:29 -0300

    Exercicio 10 corrigido e exercicio 11 terminado.

--- fundamentos/bloco-04-introducao-a-javascript-e-lógica-de-programacao/dia-1-javascript-primeiros-passos/exercicio10.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-lógica-de-programacao/dia-1-javascript-primeiros-passos/exercicio10.js
@@ -3,15 +3,15 @@ const valorDeVenda = 3000;
 
 let lucro = valorDeVenda - custoDoProduto;
 
- let unidades = 1000;
+let unidades = 1000;
 
- if (unidades < 0 ) { 
+if (unidades < 0 ) { 
   console.log("Número inválido!");
- }
- else if (unidades == 1000) {
-  console.log("Lucro %d", unidades * lucro);
 }
- /* O código calcula o lucro obtido pela venda de 
- 1000 produtos. Se a variável unidades 
- for negativa o programa 
- exibe uma mensagem de erro e é encerrado .*/
+else if (unidades == 1000) {
+  console.log("Lucro R$%d", unidades * lucro);
+}
+/* O código calcula o lucro obtido pela venda de 
+1000 produtos. Se a variável unidades 
+for negativa o programa 
+exibe uma mensagem de erro e é encerrado .*/
