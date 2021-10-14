1005ef697c1eebe26e8b7d1ad0347da08d3a5c42
Lucas <bithome1@outlook.com>
2021-10-14 20:26:23 -0300

    Exercicio 7 completo

--- fundamentos/bloco-04-introducao-a-javascript-e-lógica-de-programacao/dia-2-javascript-array-e-loop-for/menor_valor.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-lógica-de-programacao/dia-2-javascript-array-e-loop-for/menor_valor.js
@@ -1,13 +1,13 @@
-let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let numbers = [2, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 let sum = 0;
 let mediaAritmetica = 0;
 let comprimento = numbers.length;
 let store = 0;
 
-/*Encontra maior valor */
+/*Encontra menor valor */
 for (index = 0; index < comprimento; index += 1) {
   for (index1 = 0; index1 < comprimento; index1 += 1) {
-    if (numbers[index] >= numbers[index1]) {
+    if (numbers[index] <= numbers[index1]) {
       store = numbers[index];
     }
     else {
