d56c798b4766d3b3ea6cd3e2e60ee1b262bec519
Lucas <bithome1@outlook.com>
2021-10-15 13:28:21 -0300

    Exercicio 1 compelto

---
+++ fundamentos/bloco-04-introducao-a-javascript-e-lógica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/exercicio1.js
@@ -0,0 +1,8 @@
+n = 5;
+for( let b = 0; b < n; b += 1) {
+  for(let b1 = 0; b1 < n; b1 += 1) {
+    process.stdout.write('* ');
+  }
+  console.log();
+}
+/* O código constrói um quadrado de linhas. */
