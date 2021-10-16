83b2c55ef751fe860c378e3fb75d36c17089c6e5
Lucas <bithome1@outlook.com>
2021-10-16 15:08:25 -0300

    Finalizando exercicio 4

--- fundamentos/bloco-04-introducao-a-javascript-e-lógica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/exercicio3.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-lógica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/exercicio3.js
@@ -1,10 +1,10 @@
-let n = 10;
+let n = 5;
 let n2 = 0;
-for( let begin = 1,  n1 = 0; b <= n; begin += 1, n1 += 1) {
+for( let begin = 1,  n1 = 0; begin <= n; begin += 1, n1 += 1) {
   n2 = n - n1;/* Auxilia a construir espaços vazios em conjunto
   com a estrutura if . */
   //console.log(n2);
-    for(let begin1 = 1; beng1 <= n; begin1 += 1) {
+    for(let begin1 = 1; begin1 <= n; begin1 += 1) {
       if (begin1 < n2) {
         process.stdout.write(' ');
       }else{
