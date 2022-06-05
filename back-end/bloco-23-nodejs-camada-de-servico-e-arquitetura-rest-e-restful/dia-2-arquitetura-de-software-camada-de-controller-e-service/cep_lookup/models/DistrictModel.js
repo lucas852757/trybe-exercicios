cac390dfb8de262b81797f0e6a7c2c51e53f662e
Lucas <bithome1@outlook.com>
2022-06-05 17:02:45 -0300

    add express

--- back-end/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-2-arquitetura-de-software-camada-de-controller-e-service/cep_lookup/models/DistrictModel.js
+++ back-end/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-2-arquitetura-de-software-camada-de-controller-e-service/cep_lookup/models/DistrictModel.js
@@ -4,27 +4,25 @@
 const connection = require('./connection');
 
 // Criamos uma função que irá retornar o ID de um bairro
+const getDistrictId =  async (bairro, localidade, uf) => {
+   const selectQuery = 'select * from bairros where bairro = ?';
 
-const getDistrictId = async(bairro, localidade, uf) => {
-  const selectQuery = 'SELECT * FROM bairro WHERE  bairro = ?';
+   //Caso o bairro não esteja cadastrado
+   //precisamos de uma query para cadastrá - lo
 
-  // Caso o bairro não esteja cadastrado,
-  // precisamos de uma query para cadastrá-lo
-  const insertQuery = 'INSERT INTO bairros (bairro, localidade, uf) VALUES (?,?,?)';
+   const insertQuery = 'insert into bairros (bairro, localidade, uf) values (?,?,?)';
 
-  //Buscamos primeirto o bairro na tabela
-  const districtOnDB = await connection.execute(selectQuery, [bairro]);
+   //Buscamos primeiro o bairro na tela
+   const [districtOnDB] = await connection.execute(selectQuery, [bairro, localidade, uf]);
 
-  // Caso não haja resultados, cadastraremos o bairro e retirnarmos seu id
-
-  if(districtOnDB[0].length === 0) {
-    await connection.execute(insertQuery, [bairro, localidade, uf]);
-    const newDistrict = await connection.execute(selectQuery, [bairro]);
-    return newDistrict[0][0].id // propriedade id de um objeto
-  }
-
-  // Caso contrário, retornamos o id do bairro encontrado
-  return districtOnDB[0][0].id;
-};
+   // Caso não haja resultados, cadastraremos o bairro e retornaremos seu id
+   if(districtOnDB.length === 0){
+     await connection.execute(insertQuery, [bairro, localidade, uf]);
+     const [newDistrict] = await connection.execute(selectQuery, [bairro]);
+     return newDistrict[0].id;
+   }
+   // Caso contrário retornaremos o id do bairro encontrado
+   return districtOnDB[0].id;
+}
 
 module.exports = getDistrictId;
