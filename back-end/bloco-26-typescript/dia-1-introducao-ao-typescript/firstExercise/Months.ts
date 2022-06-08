d8e1beeb6d1069046baba1c5a653b3114e19b66a
Lucas <bithome1@outlook.com>
2022-06-08 14:55:22 -0300

    exercício 2 completo

---
+++ back-end/bloco-26-typescript/dia-1-introducao-ao-typescript/firstExercise/Months.ts
@@ -0,0 +1,16 @@
+enum months {
+  Janeiro = 'Janeiro',
+  Fevereiro= 'Fevereiro',
+  Marco= 'Março',
+  Abril= 'Abril',
+  Maio= 'Maio',
+  Junho= 'Junho',
+  Julho= 'Julho',
+  Agosto= 'Agosto',
+  Setembro = 'Setembro',
+  Outubro = 'Outubro',
+  Novembro = 'Novembro',
+  Dezembro = 'Dezembro'
+};
+
+export default months;
