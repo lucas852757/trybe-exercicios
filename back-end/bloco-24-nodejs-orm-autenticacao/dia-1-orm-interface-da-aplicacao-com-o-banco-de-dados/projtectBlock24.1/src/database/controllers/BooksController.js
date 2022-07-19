ded4c75387e17f1a82ed2f29db671abd7ece1ce8
Lucas <bithome1@outlook.com>
2022-07-19 19:52:47 -0300

    criado método getById

--- back-end/bloco-24-nodejs-orm-autenticacao/dia-1-orm-interface-da-aplicacao-com-o-banco-de-dados/projtectBlock24.1/src/database/controllers/BooksController.js
+++ back-end/bloco-24-nodejs-orm-autenticacao/dia-1-orm-interface-da-aplicacao-com-o-banco-de-dados/projtectBlock24.1/src/database/controllers/BooksController.js
@@ -1,4 +1,4 @@
-const booksServices = require('../services/BooksServices')
+const BooksServices = require('../services/BooksServices')
 
 /**
  * 
@@ -7,10 +7,22 @@ const booksServices = require('../services/BooksServices')
  * @param {import('express').NextFunction} next 
  */
 const getAll = async (req, res, next) => {
-  const book = await booksServices.getAll();
+  const book = await BooksServices.getAll();
   res.status(200).json(book);
 };
 
+/**
+ * 
+ * @param {import('express').Request} req 
+ * @param {import('express').Response} res 
+ * @param {import('express').NextFunction} next 
+ */
+const getById = async (req, res, next) => {
+  const {id} = req.params;
+  const book = await BooksServices.getById(id);
+  res.status(200).json(book);
+}
 module.exports = {
   getAll,
+  getById,
 }
