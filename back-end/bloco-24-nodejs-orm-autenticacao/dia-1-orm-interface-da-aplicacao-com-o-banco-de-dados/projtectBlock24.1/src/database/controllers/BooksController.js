a3b57ceef42d6c257be872e0e14d564d8a09f7ae
Lucas <bithome1@outlook.com>
2022-07-19 20:11:28 -0300

    criado tratamento para status 404

--- back-end/bloco-24-nodejs-orm-autenticacao/dia-1-orm-interface-da-aplicacao-com-o-banco-de-dados/projtectBlock24.1/src/database/controllers/BooksController.js
+++ back-end/bloco-24-nodejs-orm-autenticacao/dia-1-orm-interface-da-aplicacao-com-o-banco-de-dados/projtectBlock24.1/src/database/controllers/BooksController.js
@@ -20,6 +20,8 @@ const getAll = async (req, res, next) => {
 const getById = async (req, res, next) => {
   const {id} = req.params;
   const book = await BooksServices.getById(id);
+
+  if(!book) return res.status(404).json({message: 'Book not found'})
   res.status(200).json(book);
 }
 module.exports = {
