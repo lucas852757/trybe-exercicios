12ff377245d7fa3076529dee6041c10e32ce3d53
Lucas <bithome1@outlook.com>
2022-07-19 21:26:50 -0300

    criado método update

--- back-end/bloco-24-nodejs-orm-autenticacao/dia-1-orm-interface-da-aplicacao-com-o-banco-de-dados/projtectBlock24.1/src/database/services/BooksServices.js
+++ back-end/bloco-24-nodejs-orm-autenticacao/dia-1-orm-interface-da-aplicacao-com-o-banco-de-dados/projtectBlock24.1/src/database/services/BooksServices.js
@@ -16,8 +16,15 @@ const create = async ({title, author, pageQuantity }) => {
   const book = await Book.create({title, author, pageQuantity });
   return book;
 }
+
+const update = async(id, {title, author, pageQuantity }) => {
+  const [update] = await Book.update(id, {title, author, pageQuantity }, {where: {id}});
+
+  return update;
+}
 module.exports = {
   getAll,
   getById,
   create,
+  update,
 }
