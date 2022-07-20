ac77f6eae38c36b1059e7bc137194e303b1426b2
Lucas <bithome1@outlook.com>
2022-07-20 12:37:17 -0300

    adicinonado publisher

--- back-end/bloco-24-nodejs-orm-autenticacao/dia-1-orm-interface-da-aplicacao-com-o-banco-de-dados/projtectBlock24.1/src/database/models/book.js
+++ back-end/bloco-24-nodejs-orm-autenticacao/dia-1-orm-interface-da-aplicacao-com-o-banco-de-dados/projtectBlock24.1/src/database/models/book.js
@@ -7,6 +7,7 @@ const Book = (sequelize, DataTypes) => {
   const Book = sequelize.define('Book', {
     title: DataTypes.STRING,
     author: DataTypes.STRING,
+    publisher: DataTypes.STRING,
     pageQuantity: DataTypes.INTEGER,
     createdAt: DataTypes.DATE,
     updatedAt: DataTypes.DATE
