ad940f673aa1bcccbfd58414ab3a01205ce8a431
Lucas <bithome1@outlook.com>
2022-08-02 21:33:32 -0300

    alterações

--- back-end/bloco-25-deployment/dia-1-infraestrutura-deploy-com-heroku/deploy/index.js
+++
@@ -1,7 +0,0 @@
-const express = require('express')
-
-const app = express()
-const port = process.env.PORT || 3000
-
-app.get('/', (req, res) => res.send('Está vivo!!!'))
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
